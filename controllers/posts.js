c615ce916e69a56c48bf4a0a531bde999b309e48
mollyexten <molly.exten@gmail.com>
2021-09-09 14:50:39 -0400

    modified update controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -32,13 +32,13 @@ export const createPost = async (req, res) => {
 }
 
 export const updatePost = async (req, res) => {
-  const { id } = req.params;
-  const post = await Post.findByIdAndUpdate(id, req.body, (error, content) => {
-    if (error) {
-      return res.status(500).json({ error: error.message })
-    }
-  })
-  res.status(200).json(post);
+  try {
+    const { id } = req.params;
+    const post = await Post.findByIdAndUpdate(id, req.body);
+    res.status(200).json(post)
+    } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
 }
 
 export const deletePost = async (req, res) => {
